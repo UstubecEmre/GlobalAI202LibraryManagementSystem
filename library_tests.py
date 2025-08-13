ce7ba9dddb0e66945ba6128e04da7d66c471c6f3
UstubecEmre <emresb1999@gmail.com>
2025-08-13 22:11:51 +0300

    chore: add test_library skeleton for library test cases

---
+++ library_tests.py
@@ -0,0 +1,28 @@
+# import pytest
+import pytest
+import json
+import httpx
+from unittest.mock import patch, Mock
+from library import Library
+from book_oop import Book
+
+
+    
+def test_book_add():
+    pass 
+
+def test_remove_book():
+    pass 
+
+def test_list_books():
+    pass
+
+def test_find_book():
+    pass
+
+
+def test_load_books():
+    pass
+
+def test_save_books():
+    pass 
