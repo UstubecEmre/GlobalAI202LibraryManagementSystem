48c97e78d8845901d1098fb8bca6f02f08d657f3
UstubecEmre <emresb1999@gmail.com>
2025-08-13 11:59:01 +0300

    chore: add Library class skeleton with constructor and method definitions

---
+++ library.py
@@ -0,0 +1,23 @@
+#%% Create a library class with its methods
+#%% 2. Create a Library Class
+class Library():
+    def __init__(self):
+        self._book_lists = []
+        
+    def add_book(self, book):
+        pass
+
+    def remove_book(self, ISBN):
+        pass
+    
+    def list_books(self):
+        pass
+    
+    def find_book(self, ISBN):
+        pass
+    
+    def load_books(self):
+        pass
+    
+    def save_books(self):
+        pass 
