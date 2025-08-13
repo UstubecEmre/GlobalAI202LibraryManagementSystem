dff11dd8ed4455e9212105cf02dcf5cd9950715a
UstubecEmre <emresb1999@gmail.com>
2025-08-13 22:15:37 +0300

    chore: add create_library_sample helper

--- library_tests.py
+++ library_tests.py
@@ -6,6 +6,16 @@ from unittest.mock import patch, Mock
 from library import Library
 from book_oop import Book
 
+def create_library_sample():
+    library = Library(file_name= "library.json")
+    library._book_lists = [] # set null list
+    # create a new Book instance
+    test_book = Book("12345679123","Test Book", "Test Author")
+    
+    # append book_lists
+    library._book_lists.append(test_book)
+    
+    return library 
 
     
 def test_book_add():
