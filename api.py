dda511a39187a78542deacf0d419854192a32f3f
UstubecEmre <emresb1999@gmail.com>
2025-08-16 11:08:12 +0300

    feat(app): add lifespan() method to handle application startup and shutdown events

--- api.py
+++ api.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, Body, Query, Path
 from pydantic import BaseModel, Field
 from library import Library
 
+import asyncio
+from contextlib import asynccontextmanager
 #%% create pydantic Book class
 
 class Book(BaseModel):
@@ -13,8 +15,38 @@ class Book(BaseModel):
 
 
 
+
+
+
+
+#%% create lifespan for FastAPI
+
+""" Handle application startup and shutdown events. (Baslatma ve kapatmayi yonetecekti)"""
+# use asynccontextmanager decorator
+@asynccontextmanager
+async def lifespan(app: FastAPI){
+    """code that will run when the application starts (Uygulama baslarken calisacak kodlar)"""
+    
+    # __init__ method already running load_books() method (Zaten load_books() methodunu cagiracak ) 
+    print("Application is running")
+    
+    yield # run (calisacak)
+    
+    print("This application is shutting down (Uygulama Kapatiliyor)). Books are saving")
+    
+    # save books
+    library_instance.save_books()
+    print("Books saved succesfully (Kitaplar Basarili Bir Sekilde Kaydedildi)")
+    
+}
+ 
 #%% create an instance
-app = FastAPI()
+app = FastAPI(
+    "title":"Emre Ustubec Basic Library Management System",
+    "desciption": "A Basic FastAPI Example",
+    "version":"1.0.0",
+    "lifespan": lifespan
+)
 
 # create an instance from Library
 library_instance = Library()
@@ -58,4 +90,5 @@ def delete_book_by_ISBN(ISBN: str):
     if result:
         return {"message":"Book removed successfully (Kitap basarili bir sekilde silindi)"}
     return {"error": "Book Not Found For Deletion (Silmek Icin Kitap Bulunamadi)"}
+
 
