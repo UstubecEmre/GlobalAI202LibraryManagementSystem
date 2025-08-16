a8962dc9e25dfdf86be4730b1a416103311b44ba
UstubecEmre <emresb1999@gmail.com>
2025-08-16 11:32:22 +0300

    feat(api): add response_model for response validation

--- api.py
+++ api.py
@@ -14,6 +14,9 @@ from pydantic import BaseModel, Field
 from library import Library
 import asyncio
 from contextlib import asynccontextmanager
+
+from typing import List # for response_model
+
 #%% create pydantic Book class
 
 class Book(BaseModel):
@@ -33,7 +36,7 @@ class Book(BaseModel):
 """ Handle application startup and shutdown events. (Baslatma ve kapatmayi yonetecekti)"""
 # use asynccontextmanager decorator
 @asynccontextmanager
-async def lifespan(app: FastAPI){
+async def lifespan(app: FastAPI):
     """code that will run when the application starts (Uygulama baslarken calisacak kodlar)"""
     
     # __init__ method already running load_books() method (Zaten load_books() methodunu cagiracak ) 
@@ -47,7 +50,7 @@ async def lifespan(app: FastAPI){
     library_instance.save_books()
     print("Books saved succesfully (Kitaplar Basarili Bir Sekilde Kaydedildi)")
     
-}
+
  
 #%% create an instance
 app = FastAPI(
@@ -60,10 +63,12 @@ app = FastAPI(
 # create an instance from Library
 library_instance = Library()
 
-@app.get("/books")
+@app.get("/books", response_model = List[Book])
 def get_books():
-    books = [book.__dict__ for book in library_instance._book_lists]
-    return {"books":books}
+    # if you are not going to use response_model, you can use these codes (Eger response_model yani yanitin nasil olması gerektigini belirleme kullanmayacaksan)
+    # books = [book.__dict__ for book in library_instance._book_lists]
+    # return {"books":books}
+    return library_instance._book_lists
 
 """ 
 @app.get("/books/{ISBN}")
@@ -77,11 +82,13 @@ def get_book_by_ISBN(ISBN):
 """ 
 
 
-@app.get("/books/{ISBN}")
+@app.get("/books/{ISBN}", response_model = Book)
 def get_book_by_ISBN(ISBN: str):
     book = library_instance.find_book(ISBN.replace("-",""))
     if book:
-        return {"book":book.__dict__} # convert to JSON object
+        # if you are not going to use response_model, you can use this code
+        # return {"book":book.__dict__} # convert to JSON object
+        return book # response_model
     else:
         raise HTTPException(
             status = status.HTTP_404_NOT_FOUND,
