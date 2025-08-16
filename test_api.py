838793ff1d6f12e48073a87b585053f329d40068
UstubecEmre <emresb1999@gmail.com>
2025-08-16 20:30:31 +0300

    test: add test_add_book_manually to validate adding book

--- test_api.py
+++ test_api.py
@@ -10,6 +10,7 @@ from api import (app,
 
 client = TestClient(app)
 
+# test add_book_by_ISBN => post endpoint
 def test_add_book_by_ISBN():
     ISBN = "978-605-384-535-5"
     response = client.post(f"/books/{ISBN}")
@@ -27,3 +28,24 @@ def test_add_book_by_ISBN():
     
     
     
+#%% test manuelly add book
+# using body
+def test_add_book_manually():
+    ISBN = "978-605-384-433-4"
+    payload = {"ISBN": ISBN}
+    
+    # act (eylem)
+    response = client.post("/books", json = payload)
+    
+    # assert status_code(dogrula)
+    assert response.status_code == status.HTTP_201_CREATED
+    
+    created_book = response.json()
+    book = library_instance.find_book(ISBN)
+    
+    # assert
+    assert created_book["ISBN"] == book["ISBN"]
+    assert created_book["title"] == book["title"]
+    assert created_book["author"] == book["author"]
+     
+     
